c36a3c7174cd564e1380fcda7e34874c7820c506
Harry Winkler <harry@fumblebee.site>
2024-08-27 16:49:23 -0400

    complete part 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CS1051
+
+This is a repo keeping track of my Lab assignments and other coursework for the 
+CS 1051 course at Temple University.
---
+++ lab1/hello.py
@@ -0,0 +1,16 @@
+computer_scientist = '''
+Grace Hopper (December 09, 1906 - January 01, 1992) was a computer scientist and 
+US Navy rear admiral known for developing the first compiler, working on defining 
+the COBOL language, and coining the term 'debugging' after locating an actual dead 
+moth in a relay causing errors in a machine at Harvard.
+'''
+
+computer_scientist = '''
+Geraldine Brennan was a New Jersey based computer scientist known for working on
+the control software for the MIM-104 Patriot Missile System.  This system monitors 
+incoming threats via radar and allows users to intercept and disable incoming ballistic 
+missiles or aircraft. This was first deployed in 1981, and is still widely used 
+in defensive capablilities around the world.  She also loved the restaurant Olive Garden. 
+'''
+
+print(computer_scientist)
