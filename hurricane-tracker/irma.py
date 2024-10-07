5b99e5a540fbf3674031521cb075a440cffd4950
Harry Winkler <harry@fumblebee.site>
2024-10-07 13:12:55 -0400

    irma.py completed

--- hurricane-tracker/irma.py
+++ hurricane-tracker/irma.py
@@ -1,5 +1,5 @@
 import turtle
-
+# import csv
 
 def irma_setup():
     """Creates the Turtle and the Screen with the map background
@@ -32,13 +32,64 @@ def irma_setup():
 
     return (t, wn, map_bg_img)
 
+def get_category_storm(wind_speed: int) -> int:
+    '''Returns the storm category 0-5
+    '''
+    if wind_speed > 0 and wind_speed < 74:
+        return 0
+    elif wind_speed < 96:
+        return 1
+    elif wind_speed < 111:
+        return 2
+    elif wind_speed < 130:
+        return 3
+    elif wind_speed < 157:
+        return 4
+    elif wind_speed >= 158:
+        return 5
+    else:
+        return 0
+
+def get_storm_color(category: int) -> str:
+    '''
+    Red for Category 5
+    Orange for Category 4
+    Yellow for Category 3
+    Green for Category 2
+    Blue for Category 1
+    White if not hurricane strength
+    '''
+    color_dict = {
+        5: 'red',
+        4: 'orange',
+        3: 'yellow',
+        2: 'green',
+        1: 'blue',
+        0: 'white'
+    }
+    return color_dict[category]
+
 
 def irma():
     """Animates the path of hurricane Irma
     """
+    import csv
     (t, wn, map_bg_img) = irma_setup()
+    t.pendown()
 
-    # your code to animate Irma here
+    with open('data/irma.csv', 'r') as file:
+        reader = list(csv.reader(file))
+        for i in range(1,len(reader)):
+            # Lat is 2, Lon is 3, Wind Speed is 4
+            (lat, lon, wind) = float(reader[i][2]), float(reader[i][3]), int(reader[i][4])
+
+            # draw frame
+            category = get_category_storm(wind)
+            t.color(get_storm_color(category))
+            t.pensize(category**1.2)
+            t.setpos(lon, lat)
+
+    wn.mainloop()
 
 if __name__ == "__main__":
     irma()
